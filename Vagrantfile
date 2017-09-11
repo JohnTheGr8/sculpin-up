17a2fa480f6b6b2ca73a8199d6ebd3deae298ab1
Ioannis G <jtgftw@gmail.com>
2017-09-11 17:12:12 +0300

    clone and serve sculpin-blog-skeleton

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,11 @@ Vagrant.configure("2") do |config|
   config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
   config.vm.provider "virtualbox"
 
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+    v.cpus = 2
+  end
+
   config.vm.network "private_network", ip: "192.168.56.100", adapter: 2
 
   config.vm.provision "shell", path: "setup_system.sh"
--- setup_sculpin.sh
+++ setup_sculpin.sh
@@ -9,3 +9,19 @@ wget https://raw.githubusercontent.com/composer/getcomposer.org/1b137f8bf6db3e79
 mkdir -p ~/bin
 mv composer.phar ~/bin/composer
 chmod u+x ~/bin/composer
+
+###
+# Clone sculpin blog skeleton
+###
+
+git clone https://github.com/sculpin/sculpin-blog-skeleton.git www
+
+cd www
+
+~/bin/composer install
+
+###
+# Generate sculpin site
+###
+
+vendor/bin/sculpin generate --env=dev
